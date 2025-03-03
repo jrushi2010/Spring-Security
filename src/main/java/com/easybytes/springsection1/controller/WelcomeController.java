065683abc0d081881228ba9a2666844d6f85672d
jrushi2010 <jrushi2010@gmail.com>
2025-03-03 18:52:41 +0530

    Project created

---
+++ src/main/java/com/easybytes/springsection1/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.easybytes.springsection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String sayWelcome() {
+		return "Welcome to spring Application without security";
+	}
+}
